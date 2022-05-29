fce68774d3f7a16370acca1f2925bdfb8ab45a07
wangwei <wangwei04@inke.cn>
2022-05-29 20:46:23 +0800

    first commit

---
+++ MMMiddleMan.h
@@ -0,0 +1,16 @@
+//
+//  MMMiddleMan.h
+//  Test
+//
+//  Created by wangwei on 2022/5/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MMMiddleMan : NSURLProtocol
+
+@end
+
+NS_ASSUME_NONNULL_END
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MMMiddleMan
 Use NSURLProtocol realize ip direct connection
+
+- ip直连解决方案
+- 依赖腾讯开源HTTPDNS
+- 解决了无法抓包的问题
+- 支持SNI
+- 支持证书认证逻辑
